8df2aade5362be44eb1f8c1b99325cff2a6a72bc
Dhanya <11249a034@kanchiuniv.ac.in>
2026-08-05 11:43:09 +0530

    Add files via upload

---
+++ staticDemo.java
@@ -0,0 +1,8 @@
+class staticDemo{
+    static void display(){
+        System.out.println("this is a static method.");
+    }
+    public static void main(String[]args){
+        staticDemo.display();
+    }
+}
