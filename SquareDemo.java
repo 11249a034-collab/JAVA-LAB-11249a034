4d3a7940e497b1c095c41d3de4ee578743ca8936
Dhanya <11249a034@kanchiuniv.ac.in>
2026-08-05 11:41:52 +0530

    Add files via upload

---
+++ SquareDemo.java
@@ -0,0 +1,8 @@
+class SquareDemo{
+    Static void Square(int n){
+        System.out.println("Square of "+n+"="(n*n));
+    }
+    public static void main(string[]args){
+        SquareDemo.Square(7);
+    }
+}
